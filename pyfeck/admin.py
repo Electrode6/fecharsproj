814056d824c795c787e7be5efebcf5a24e6b5ce7
Electrode6 <dsduma99@gmail.com>
2016-04-10 17:55:47 +0100

    Finished class models version 1

--- pyfeck/admin.py
+++ pyfeck/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from pyfeck.models import Weapon, WeaponRank, StatCategory, CharacterClassCategory, CharacterClassStat, \
-    CharacterClass, CompoundStatCategory, CharacterClassCompoundStat, CharacterClassWeapon, CharacterClassPromotion, CharacterClassSkill
+    CharacterClass, CompoundStatCategory, CharacterClassCompoundStat, CharacterClassWeapon, CharacterClassPromotion, CharacterClassSkill, Character, \
+    SexCategory, CharacterStat, CharacterWeaponRank, SupportLevelAvailability, SupportLevel, \
+    CharacterSupportLevelStatBonus, CharacterRelationship
+
 
 class CharacterClassPromotion_Inline(admin.TabularInline):
     model = CharacterClassPromotion
@@ -45,10 +48,47 @@ class CharacterClassSkill_Inline(admin.TabularInline):
     model = CharacterClassSkill
     extra = 2
 
+class CharacterStat_Inline(admin.TabularInline):
+    model = CharacterStat
+    extra = 9
+
+class CharacterWeaponRank_Inline(admin.TabularInline):
+    model = CharacterWeaponRank
+    extra = 3
+
+class CharacterSupportLevelStatBonus_Inline(admin.TabularInline):
+    model = CharacterSupportLevelStatBonus
+    extra = 1
+
+class CharacterRelationship_Inline(admin.TabularInline):
+    model = CharacterRelationship
+    extra = 1
+    fk_name = "supportingCharacter"
+
 class CharacterClassAdmin(admin.ModelAdmin):
     fieldsets = [("Generic", {"fields" : ["category", "tier"]})]
     inlines = [CharacterClassStat_Inline, CharacterClassCompoundStat_Inline, CharacterClassWeapon_Inline, CharacterClassPromotion_Inline, CharacterClassSkill_Inline]
 
+class CharacterAdmin(admin.ModelAdmin):
+    fieldsets = [("Generic", {"fields" : ["name", "gender", "baseLevel", "skillName", "skillDescription", "primaryClassCategory", "secondaryClassCategory"]})]
+    inlines = [CharacterStat_Inline, CharacterWeaponRank_Inline, CharacterSupportLevelStatBonus_Inline, CharacterRelationship_Inline]
+
+class SupportLevelAvailability_Inline(admin.TabularInline):
+    model = SupportLevelAvailability
+    extra = 1
+
+class SupportLevel_Inline(admin.TabularInline):
+    model = SupportLevel
+    extra = 1
+
+class GenderAdmin(admin.TabularInline):
+    model = SexCategory
+    extra = 1
+
+admin.site.register(SupportLevel)
+admin.site.register(SupportLevelAvailability)
+admin.site.register(SexCategory)
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(CompoundStatCategory)
 admin.site.register(CharacterClass, CharacterClassAdmin)
 admin.site.register(CharacterClassCategory)
--- pyfeck/models.py
+++ pyfeck/models.py
@@ -28,6 +28,10 @@ class CharacterClassCategory(models.Model):
     def __str__(self):
         return self.name
 
+class CharacterClassCategoryRelation(models.Model):
+    fromClassCategory = models.ForeignKey(CharacterClassCategory, on_delete = models.CASCADE, related_name = "from_Class_Category")
+    toClassCategory = models.ForeignKey(CharacterClassCategory, on_delete = models.CASCADE, related_name = "to_Class_Category")
+
 class CompoundStatCategory(models.Model):
     name = models.CharField(max_length = 20)
     class Meta:
@@ -53,6 +57,8 @@ class CharacterClassStat(models.Model):
     characterClass = models.ForeignKey(CharacterClass, on_delete = models.CASCADE)
     stat = models.ForeignKey(StatCategory, on_delete = models.CASCADE)
     growth = models.IntegerField()
+    cap = models.IntegerField()
+    guardStanceBonus = models.IntegerField()
     def __str__(self):
         return self.characterClass.category.name + " - " + self.stat.name
 
@@ -77,8 +83,27 @@ class CharacterClassSkill(models.Model):
     def __str__(self):
         return self.name
 
+class SupportLevelAvailability(models.Model):
+    description = models.CharField(max_length = 20)
+    class Meta:
+        verbose_name = "Support Level Availability"
+        verbose_name_plural = "Support Level Availabilities"
+    def __str__(self):
+        return self.description
+
+class SupportLevel(models.Model):
+    name = models.CharField(max_length = 2)
+    availability = models.ForeignKey(SupportLevelAvailability, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.name
+
 class SexCategory(models.Model):
     name = models.CharField(max_length = 10)
+    class Meta:
+        verbose_name = "Sex Category"
+        verbose_name_plural = "Sex Categories"
+    def __str__(self):
+        return self.name
 
 class Character(models.Model):
     name = models.CharField(max_length = 20)
@@ -86,25 +111,35 @@ class Character(models.Model):
     baseLevel = models.IntegerField()
     skillName = models.CharField(max_length = 50)
     skillDescription = models.CharField(max_length = 500)
-    primaryClass = models.ForeignKey(CharacterClass, on_delete = models.CASCADE)
-    # secondaryClass = models.ForeignKey()
+    currentClass = models.ForeignKey(CharacterClass, on_delete = models.CASCADE)
+    primaryClassCategory = models.ForeignKey(CharacterClassCategory, on_delete = models.CASCADE, related_name = "primary_Class_Category")
+    secondaryClassCategory = models.ForeignKey(CharacterClassCategory, on_delete = models.CASCADE, related_name = "secondary_Class_Category")
+    isCurrentClassPrimary = models.BooleanField()
 
-class CharacterStatGrowth(models.Model):
+class CharacterSupportLevelStatBonus(models.Model):
+    character = models.ForeignKey(Character, on_delete = models.CASCADE)
+    supportLevel = models.ForeignKey(SupportLevel, on_delete = models.CASCADE)
+    stat = models.ForeignKey(StatCategory, on_delete = models.CASCADE)
+    attackStanceBonus = models.IntegerField()
+    guardStanceBonus = models.IntegerField()
+
+class CharacterRelationship(models.Model):
+    supportingCharacter = models.ForeignKey(Character, on_delete = models.CASCADE, related_name = "supporting_Character")
+    supportedCharacter = models.ForeignKey(Character, on_delete = models.CASCADE, related_name = "supported_Character")
+    supportLevel = models.ForeignKey(SupportLevel, on_delete = models.CASCADE)
+    dependentCharacter = models.ForeignKey(Character, on_delete = models.CASCADE, related_name = "dependent_Character")
+
+class CharacterWeaponRank(models.Model):
+    weapon = models.ForeignKey(Weapon, on_delete = models.CASCADE)
+    base_weapon_rank = models.ForeignKey(WeaponRank, on_delete = models.CASCADE)
+    character = models.ForeignKey(Character, on_delete = models.CASCADE)
+
+class CharacterStat(models.Model):
     character = models.ForeignKey(Character, on_delete = models.CASCADE)
     stat = models.ForeignKey(StatCategory, on_delete = models.CASCADE)
     growth = models.IntegerField()
+    base = models.IntegerField()
+    cap_modifier = models.IntegerField()
 
-class CharacterStatBase(models.Model):
-    character = models.ForeignKey(Character, on_delete = models.CASCADE)
-    stat = models.ForeignKey(StatCategory, on_delete = models.CASCADE)
-    value = models.IntegerField()
-
-# TODO: Add base character weapon ranks
-# TODO: Add stat cap modifiers
-# TODO: Add class stat caps
-# TODO: Add character stat growths and base stats
-# TODO: Add each characters' supportables
-# TODO: Add AS and GS bonuses and tie to support levels
-# TODO: Add character secondary classes, friendship classes, marriage classes
+# TODO: Add character friendship classes, marriage classes
 # TODO: Add child characters and make Gen 2 concept
-#
